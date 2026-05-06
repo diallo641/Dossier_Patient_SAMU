28b0f0f2a40ee4f329909aaf3b30b95806409324
Boubacar DIALLO <boubacar95@outlook.fr>
2026-05-06 10:45:36 +0000

    Securiser les routes pour la table role

--- Backend/routes/roleRoutes.js
+++ Backend/routes/roleRoutes.js
@@ -1,26 +1,29 @@
 const express = require("express");
 const router = express.Router();
 
+const authMiddleware = require("../middlewares/authMiddleware");
+const roleMiddleware = require("../middlewares/roleMiddleware");
+
 const roleController = require("../controllers/roleController");
 
 
 //LISTER TOUS LES ROLES
-router.get("/roles", roleController.getAllRoles);
+router.get("/roles", authMiddleware, roleMiddleware("Admin"), roleController.getAllRoles);
 
 
 //OBTENIR UN ROLE PAR ID
-router.get("/roles/:id", roleController.getRoleById);
+router.get("/roles/:id", authMiddleware, roleController.getRoleById);
 
 
 //CREER UN ROLE
-router.post("/roles", roleController.createRole);
+router.post("/roles", authMiddleware, roleMiddleware("Admin"), roleController.createRole);
 
 
 //MODIFIER UN ROLE
-router.put("/roles/:id", roleController.updateRole);
+router.put("/roles/:id", authMiddleware, roleMiddleware("Admin"), roleController.updateRole);
 
 
 //SUPPRIMER UN ROLE
-router.delete("/roles/:id", roleController.deleteRole);
+router.delete("/roles/:id", authMiddleware, roleMiddleware("Admin"), roleController.deleteRole);
 
 module.exports = router;
